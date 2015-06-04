b9a2ab4b699abf76dbb5ff23e5c34ee6050ed414
Scott Davis <stdavis@utah.gov>
2015-06-04 11:30:45 -0600

    jscs fixes and test fix for travis

--- app/index.js
+++ app/index.js
@@ -1,55 +1,53 @@
 'use strict';
-var util = require('util');
-var path = require('path');
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 
 var DojoClassGenerator = yeoman.generators.Base.extend({
-  init: function() {
+    init: function () {
 
-  },
+    },
 
-  askFor: function() {
-    var done = this.async();
+    askFor: function () {
+        var done = this.async();
 
-    // have Yeoman greet the user
-    console.log(this.yeoman);
+        // have Yeoman greet the user
+        console.log(this.yeoman);
 
-    // replace it with a short and sweet description of your generator
-    console.log(chalk.magenta('You\'re using the fantastic dojo-class generator.'));
+        // replace it with a short and sweet description of your generator
+        console.log(chalk.magenta('You\'re using the fantastic dojo-class generator.'));
 
-    var prompts = [{
-      name: 'className',
-      message: 'Class Name:',
-      default: 'ClassName'
-    }, {
-      name: 'description',
-      message: 'Description:'
-    }, {
-      name: 'path',
-      message: 'Path to class:',
-      default: 'app'
-    }];
+        var prompts = [{
+            name: 'className',
+            message: 'Class Name:',
+            default: 'ClassName'
+        }, {
+            name: 'description',
+            message: 'Description:'
+        }, {
+            name: 'path',
+            message: 'Path to class:',
+            default: 'app'
+        }];
 
-    this.prompt(prompts, function(props) {
-      this.className = props.className;
-      this.description = props.description;
-      this.path = props.path + '/';
-      this.consoleLog = this.path + this.className;
-      this.packageName = this.path.split('/')[0];
+        this.prompt(prompts, function (props) {
+            this.className = props.className;
+            this.description = props.description;
+            this.path = props.path + '/';
+            this.consoleLog = this.path + this.className;
+            this.packageName = this.path.split('/')[0];
 
-      done();
-    }.bind(this));
-  },
+            done();
+        }.bind(this));
+    },
 
-  app: function() {
-    this.template('_class.js', this.path + this.className + '.js');
-    this.template('_spec.js', this.path + 'tests/spec/Spec' + this.className + '.js');
-  },
+    app: function () {
+        this.template('_class.js', this.path + this.className + '.js');
+        this.template('_spec.js', this.path + 'tests/spec/Spec' + this.className + '.js');
+    },
 
-  projectfiles: function() {
+    projectfiles: function () {
 
-  }
+    }
 });
 
 module.exports = DojoClassGenerator;
--- test/test-creation.js
+++ test/test-creation.js
@@ -1,39 +1,38 @@
-/*global describe, beforeEach, it */
 'use strict';
 var path = require('path');
 var helpers = require('yeoman-generator').test;
 
-describe('dojo-class generator', function() {
-  beforeEach(function(done) {
-    helpers.testDirectory(path.join(__dirname, 'temp'), function(err) {
-      if (err) {
-        return done(err);
-      }
+describe('dojo-class generator', function () {
+    beforeEach(function (done) {
+        helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+            if (err) {
+                return done(err);
+            }
 
-      this.app = helpers.createGenerator('dojo-class:app', [
-        '../../app'
-      ]);
-      done();
-    }.bind(this));
-  });
-
-  it('creates expected files', function(done) {
-    var expected = [
-      // add files you expect to exist here.
-      'app/test.js',
-      'app/tests/spec/Spectest.js'
-    ];
-
-    helpers.mockPrompt(this.app, {
-      'className': 'test',
-      'description': 'test description',
-      'path': 'app'
+            this.app = helpers.createGenerator('dojo-class:app', [
+              '../../app'
+            ]);
+            done();
+        }.bind(this));
     });
 
-    this.app.options['skip-install'] = true;
-    this.app.run({}, function() {
-      helpers.assertFile(expected);
-      done();
+    it('creates expected files', function (done) {
+        var expected = [
+          // add files you expect to exist here.
+          'app/test.js',
+          'app/tests/spec/Spectest.js'
+        ];
+
+        helpers.mockPrompt(this.app, {
+            'className': 'test',
+            'description': 'test description',
+            'path': 'app'
+        });
+
+        this.app.options['skip-install'] = true;
+        this.app.run({}, function () {
+            helpers.assertFile(expected);
+            done();
+        });
     });
-  });
 });
